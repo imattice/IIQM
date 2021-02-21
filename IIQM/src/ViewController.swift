f53178ed92a20a44609b8fffc013c651d7e5a079
Ike Mattice <ikemattice@icloud.com>
2021-02-20 17:13:07 -0800

    add a method to the ViewController to simulate the data flow

--- IIQM/src/ViewController.swift
+++ IIQM/src/ViewController.swift
@@ -9,15 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var data = [Int]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-        let iiqm = IIQM()
-//        iiqm.calculate(path: Bundle.main.path(forResource: "data", ofType: "txt")!)
+        guard let data = try? TextReader().numericValues(from: "data-20k")
+        else { showDataFailureAlert(); return }
+        
+        self.data = data
+        
+        simulateDataFlow()
+        
     }
-
-
+    
+    func showDataFailureAlert() {
+        //here is some code to show a UIAlert when the data is unavailable
+    }
+    
+    func simulateDataFlow() {
+        //start with the interquartile mean for the first 4 values
+        var currentMean = Array(data[0...3]).interquartileMean()
+        
+        for (index, _) in data.enumerated() {
+            //ignore the first 4 values
+            if index < 4 { continue }
+            
+            //crunch the interquartile mean up until the current index
+            currentMean = Array(data[0...index]).interquartileMean()
+            
+            let meanString = String(currentMean)
+            
+            print("Index => \(index), Mean => \(meanString)")
+        }
+    }
 }
-
