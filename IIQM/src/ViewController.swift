0c1863bb2a39f1f1a51c80c781ea23126ad0ca0d
Ike Mattice <ikemattice@icloud.com>
2021-02-21 10:12:59 -0800

    Add comments to show thought process while working out this issue

--- IIQM/src/ViewController.swift
+++ IIQM/src/ViewController.swift
@@ -19,8 +19,7 @@ class ViewController: UIViewController {
         
         self.data = data
         
-//        simulateDataFlow()
-        
+        simulateDataFlow()
     }
     
     func showDataFailureAlert() {
@@ -30,16 +29,86 @@ class ViewController: UIViewController {
     // Can we use recusion when new data is added?
     func simulateDataFlow() {
         //start with the interquartile mean for the first 4 values
-        var currentMean = Array(data[0...3]).interquartileMean()
+//        var currentMean = Array(data[0...3]).interquartileMean()
+//        var q1 = data[Int(data.count*1/4)]
+//        var q2 = data.mean()
+//        var q3 = data[Int(data.count*3/4)]
+        
+        
+//        Looking for patterns as data is added
+//        sample data set of 10 random numbers between 1-10
+//        [9 9 3 2 5 4 6 6 8 1]
+//
+        
+//        //look for pattern in quartiles as new numbers are added
+//        [9, 9, 3, 2].sorted = [2, 3, 9, 9]
+//        q1 = 2.5, q2=6, q3 = 9
+//        //add 5
+//        [2, 3, 5, 9, 9]
+//        q1 = 2.5, q2=5, q3 = 9
+//        Note: the count is odd and the new value is in the median quartile, so the q1 & q3 do not change
+//        //add 4
+//        [2, 3, 4, 5, 9, 9]
+//        q1 = 3, q2=4.5, q3 = 9
+//        Note: the value is between q1 and q2, so those are recalculated.  q3 stays the same
+//        add 6
+//        [2, 3, 4, 5, 6, 9, 9]
+//        q1 = 3, q2=5, q3 = 9
+//        Note: the count is odd and the new value is in the median quartile, so the q1 &q3 do not change
+//        add 6
+//        [2, 3, 4, 5, 6, 6, 9, 9]
+//        q1 = 3.5, q2=5.5, q3 = 7.5
+//        Note: the count is even and all values changed
+//        add 8
+//        [2, 3, 4, 5, 6, 6, 8, 9, 9]
+//        q1 = 3.5, q2=6, q3 = 8.5
+//        add 1
+//        [1, 2, 3, 4, 5, 6, 6, 8, 9, 9]
+//        q1 = 3, q2=5.5, q3 = 8
+//        //it doesn't look like there is a pattern we can exploit by just calculating q1, q2, and q3 and the new number
+//        //the points at which the values stay the same don't seem reliable with such a small data set
+
+        
+//        //only recalculate on the odd count?
+//        //this would be kind of difficult to understand without a lot of payoff.  When we get to the millions of values, the median will still be too large to calculate quickly
+
+        
+//        //aggregate numbers?
+//        [1, 2, 9, 10]
+//        q1 = 1.5, q3 = 9.5, m = 5.5
+//        [1, 5.5, 10]
+//        //add 7
+//        [1, 5.5, 7, 10]
+//        [1, 6.25, 10]
+//        //the values are lost over time.  This would likely be fast and also trend accurate over time, but if we are looking for exact, testable matches, this wouldn't be a good approach
+
+        
+//        //just return the average of q1 & q3?
+//        //very fast but not accurate.  Doesn't account for weight of values in the middle quartile
+        
+        
+        
+        //Results
+        //we can limit recalculating q1 and q3 to every other value
+        //if the new value is below q1 or above q3, then we should recalculate that value and then recalculate the mean
+        //We could reduce the size of the median quartile
+        //We could reduce the number of calculations
+            //batched data?
+        
+        
         
         for (index, _) in data.enumerated() {
             //ignore the first 4 values
             if index < 4 { continue }
+//            var arr = data[0...index]
+//            var q1 = arr[Int(arr.count*1/4)]
+//            var q2 = data.mean()
+//            var q3 = arr[Int(arr.count*3/4)]
             
             //crunch the interquartile mean up until the current index
-            currentMean = Array(data[0...index]).interquartileMean()
+            let mean = Array(data[0...index]).interquartileMean()
             
-            let meanString = String(currentMean)
+            let meanString = String(format: "%.2f", mean)
             
             print("Index => \(index), Mean => \(meanString)")
         }
