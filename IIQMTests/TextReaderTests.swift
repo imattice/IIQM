a42be69ff471af9feb51675750502d9ad5112fd8
Ike Mattice <ikemattice@icloud.com>
2021-02-20 16:40:16 -0800

    update TextReader test methods to use an optional try; fix an off by 1 error in the TextReader test results

--- IIQMTests/TextReaderTests.swift
+++ IIQMTests/TextReaderTests.swift
@@ -12,14 +12,17 @@ import XCTest
 class TextReaderTests: XCTestCase {
     
     func testNumericValuesFromFileName() {
-        let data = TextReader().numericValues(from: "data.txt")
-        let data20k = TextReader().numericValues(from: "data-20k.txt")
+        let rawData = try? TextReader().numericValues(from: "data")
+        let rawData20k = try? TextReader().numericValues(from: "data-20k")
+        
+        guard let data = rawData, let data20k = rawData20k
+        else { XCTFail(); return }
         
         XCTAssertEqual(data[0...3],                         [301, 286, 287, 292],   "First 4 values read from 'data.txt' are incorrect")
-        XCTAssertEqual(data[data.count-5...data.count-1],   [102, 69, 74, 75],      "Last 4 values read from 'data.txt' are incorrect")
+        XCTAssertEqual(data[data.count-4...data.count-1],   [102, 69, 74, 75],      "Last 4 values read from 'data.txt' are incorrect")
         
         XCTAssertEqual(data20k[0...3],                         [301, 286, 287, 292],   "First 4 values read from 'data-20k.txt' are incorrect")
-        XCTAssertEqual(data20k[data.count-5...data.count-1],   [522, 515, 505, 496],   "Last 4 values read from 'data-20k.txt' are incorrect")
+        XCTAssertEqual(data20k[data20k.count-4...data20k.count-1],   [522, 515, 505, 496],   "Last 4 values read from 'data-20k.txt' are incorrect")
     }
 
 }
