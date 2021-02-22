b267f0b8c6fa404febf97a21482089f717b385f8
Ike Mattice <ikemattice@icloud.com>
2021-02-21 20:10:01 -0800

    rename current initialization method to progressiveSum() to indicate the solution it is using; clean up solution comments

--- IIQM/src/ViewController.swift
+++ IIQM/src/ViewController.swift
@@ -19,102 +19,129 @@ class ViewController: UIViewController {
         
         self.data = data
         
-        simulateDataFlow()
+//        IIQM().calculate(path: "data-20k")
+        
+        progressiveSum()
     }
     
     func showDataFailureAlert() {
         //here is some code to show a UIAlert when the data is unavailable
     }
     
-    // Can we use recusion when new data is added?
-    func simulateDataFlow() {
-        //start with the interquartile mean for the first 4 values
-//        var currentMean = Array(data[0...3]).interquartileMean()
-//        var q1 = data[Int(data.count*1/4)]
-//        var q2 = data.mean()
-//        var q3 = data[Int(data.count*3/4)]
-        
-        
-//        Looking for patterns as data is added
-//        sample data set of 10 random numbers between 1-10
-//        [9 9 3 2 5 4 6 6 8 1]
+//    PROBLEM - iterating over a long list of values takes too long
+//        - This appears to be because the methods used to get the IQ mean are initializing new arrays from array subslices and generating new arrays during sort
 //
-        
-//        //look for pattern in quartiles as new numbers are added
-//        [9, 9, 3, 2].sorted = [2, 3, 9, 9]
-//        q1 = 2.5, q2=6, q3 = 9
-//        //add 5
-//        [2, 3, 5, 9, 9]
-//        q1 = 2.5, q2=5, q3 = 9
-//        Note: the count is odd and the new value is in the median quartile, so the q1 & q3 do not change
-//        //add 4
-//        [2, 3, 4, 5, 9, 9]
-//        q1 = 3, q2=4.5, q3 = 9
-//        Note: the value is between q1 and q2, so those are recalculated.  q3 stays the same
-//        add 6
-//        [2, 3, 4, 5, 6, 9, 9]
-//        q1 = 3, q2=5, q3 = 9
-//        Note: the count is odd and the new value is in the median quartile, so the q1 &q3 do not change
-//        add 6
-//        [2, 3, 4, 5, 6, 6, 9, 9]
-//        q1 = 3.5, q2=5.5, q3 = 7.5
-//        Note: the count is even and all values changed
-//        add 8
-//        [2, 3, 4, 5, 6, 6, 8, 9, 9]
-//        q1 = 3.5, q2=6, q3 = 8.5
-//        add 1
-//        [1, 2, 3, 4, 5, 6, 6, 8, 9, 9]
-//        q1 = 3, q2=5.5, q3 = 8
-//        //it doesn't look like there is a pattern we can exploit by just calculating q1, q2, and q3 and the new number
-//        //the points at which the values stay the same don't seem reliable with such a small data set
-
-        
-//        //only recalculate on the odd count?
-//        //this would be kind of difficult to understand without a lot of payoff.  When we get to the millions of values, the median will still be too large to calculate quickly
-
-        
-//        //aggregate numbers?
-//        [1, 2, 9, 10]
-//        q1 = 1.5, q3 = 9.5, m = 5.5
-//        [1, 5.5, 10]
-//        //add 7
-//        [1, 5.5, 7, 10]
-//        [1, 6.25, 10]
-//        //the values are lost over time.  This would likely be fast and also trend accurate over time, but if we are looking for exact, testable matches, this wouldn't be a good approach
-
-        
-//        //just return the average of q1 & q3?
-//        //very fast but not accurate.  Doesn't account for weight of values in the middle quartile
-        
-        
-        
-        //Results
-        //we can limit recalculating q1 and q3 to every other value
-        //if the new value is below q1 or above q3, then we should recalculate that value and then recalculate the mean
-        //We could reduce the size of the median quartile
-        //We could reduce the number of calculations
-            //batched data?
-        
-        
-        
-        for (index, _) in data.enumerated() {
+//    SOLUTIONS -
+//
+//    1) Update and sort in batches
+//            This method reduces the number of calculations, but in the end doesn't save that much time. We lose the granularity of each iteration
+//
+//    2) Value aggregation
+//              We can reduce the size of the array by replacing the values with aggregate values.  This tradees off speed for accuracy.  This also still relies on sorting arrays after the aggregation occurs, which appears to be taking up most of the time.  If we are looking for exact, testable matches, this wouldn't be a good approach
+//
+//    3) Store calculations as an index : mean key value pair, and then look up the value
+//              Would not scale well
+//
+//    4) Progressive Sum
+//              We just keep track of the sum of the median quartile, which we can then use to find the mean value we are after
+//              This would also mean that we would only be adding/subtracting at most 3 values at a time from the sum, avoiding having to sum up the entire median quartile over and over
+//              We will still need to track the values in the q1 and q3 in case the median quartile shifts in those directions, so keeping another array to hold each of those values would be necessary.  To avoid calling .sorted() each time a new value is added, we need to figure out at what index in our current array to insert the new values so that it stays sorted without (exessive?) looping or creating a new array.  Maybe some kind of search tree?
+    
+    
+    func progressiveSum() {
+        let test = [1.0, 2, 3, 4, 1, 6, 30, 28, 12, 2, 3]
+        var interquartileMean: Double = 0
+        //Since we know we start with 4 values, we can calculate the initial sum from that part of the array
+//        var medianSum: Double = data[1...2].reduce(0, {$0 + $1})
+        var initialData = [800.0]//data[0...3]
+                
+        for (index, value) in test.enumerated() { //data.enumerated() {
             //ignore the first 4 values
-            if index < 4 { continue }
-//            var arr = data[0...index]
-//            var q1 = arr[Int(arr.count*1/4)]
-//            var q2 = data.mean()
-//            var q3 = arr[Int(arr.count*3/4)]
+//            if index < 4 { continue }
+
+            //insert the new value into the array so that it stays sorted
+            var insertIndex = initialData.count //- 1//value >= initialData[initialData.count / 2] ? initialData.count / 2 : initialData.count - 1
+           
+//            Search Tree Iteration
+//            while insertIndex > 0 && value < initialData[insertIndex - 1] {
+//                if value < initialData[insertIndex] {
+//                    insertIndex = insertIndex / 2
+//                }
+//                else {
+//                    insertIndex = insertIndex
+//                }
+//                insertIndex = insertIndex / 2
+//
+//                insertIndex -= 1
+//            }
             
-            //crunch the interquartile mean up until the current index
+            
+            //TODO: - Test if building out a search tree would improve performance here
+            while insertIndex > 0 && value < initialData[insertIndex - 1] {
+                insertIndex -= 1
+            }
+            initialData.insert(value, at: insertIndex)
+            
+            dump(initialData)
+            
+            
+            ///The index at the end of the first quartile
+            let q1i = Int(initialData.count*1/4)
+            ///The index at the end of the third quartile
+            let q3i = Int(initialData.count*3/4)
+            ///The sum of the q2 and q3 quartiles
+            let medianQ = initialData[q1i...q3i]
+            
+            
+            
+
+            
+//            let mean = initialData[(initialData.count*1/4)...(initialData.count*3/4)].reduce(0) { $0 + $1 } / Double(initialData.count)
+            
+//            if value > data[index / 4] || value < data[(3*index) / 4] {
+//
+//            }
+            //if the index is odd and the value is in the median, then nothing changes
+            //add to the sum
+            
+            
+            
+            //if the index is even, we need to recalculate the bounds of the quartile
+//            if index % 2 == 0 {
+//                ///The values of the data array up to the current iterative index
+//                let arr = data[0...index].sorted()
+//                ///The index at the end of the first quartile
+//                let q1i = Int(arr.count*1/4)
+//                ///The index at the end of the third quartile
+//                let q3i = Int(arr.count*3/4)
+//                ///The sum of the q2 and q3 quartiles
+//                let medianQ = arr[q1i...q3i]
+//
+//                //if the value is not in the q1 or q4, then we need to recalcualte the interquartileMean
+//                if value > arr[q1i] || value < arr[q3i] {
+//                    interquartileMean = arr.interquartileMean() //medianQ.reduce(0, {$0 + $1} ) / Double(medianQ.count)
+//                }
+//            }
+            
+
+            let meanString = String(format: "%.2f", interquartileMean)
+            print("Index => \(index), Mean => \(meanString)")
+        }
+        
+
+    }
+    
+    func arrayExtension() {
+        for (index, _) in data.enumerated() {
             let mean = Array(data[0...index]).interquartileMean()
             
             let meanString = String(format: "%.2f", mean)
-            
             print("Index => \(index), Mean => \(meanString)")
+
         }
     }
     
-    ///A test calculation for optimizing the calculation of Interquartile Mean
+    ///A  calculation for optimizing the calculation of Interquartile Mean
     //This may be illegal based on point 3 in the optimization challenge brief: "still calculates the Incremental Interquartile Mean after each value is read"
     //this gives us inaccurate data and does not speed up the calculation significantly.
     func averageBatch() {
@@ -137,7 +164,5 @@ class ViewController: UIViewController {
             
             print("Index => \(index), Mean => \(meanString)")
         }
-        
-
     }
 }
