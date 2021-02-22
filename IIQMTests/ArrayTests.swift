81ac124195ed2489f9b4f43771a4e6f34ea8c9bb
Ike Mattice <ikemattice@icloud.com>
2021-02-21 20:08:09 -0800

    remove testRemoveInterquartileBounds() since that method is no longer being used

--- IIQMTests/ArrayTests.swift
+++ IIQMTests/ArrayTests.swift
@@ -31,28 +31,27 @@ class ArrayTests: XCTestCase {
                        "Mean of an array of negative values is incorrect")
         XCTAssertEqual(simple.mean(),       7.4,      "Mean of a simple values is incorrect")
         XCTAssertEqual(complex.mean(),      245.16,    "Mean of large values is incorrect")
-
     }
     
-    func testRemoveInterquartileBounds() {
-        let zero = [0.0, 0, 0]
-        
-        XCTAssertEqual(zero.medianQuartileValues(), [0, 0, 0], "Failed to remove interquartile bounds of empty array")
-        
-        let sorted = [1.0, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        
-        XCTAssertEqual(sorted.medianQuartileValues(), [3, 4, 5, 6, 7, 8], "Failed to remove interquartile bounds of sorted array")
-
-        
-        let simple = [10.0,  8,  2,  3,  1]
-        
-        XCTAssertEqual(simple.medianQuartileValues(), [2, 3, 8], "Failed to remove interquartile bounds of simple array")
-
-        
-        let complex = [93.0,  291,  205,  572,  175,  484,  456,  569,  33,  192,  245,  331,  126,  340,  424,  238,  156,  564,  516,  345,  195,  422,  10,  433,  112]
-        
-        XCTAssertEqual(complex.medianQuartileValues(), [175, 192, 195, 205, 238, 245, 291, 331, 340, 345, 422, 424, 433], "Failed to remove interquartile bounds of complex array")
-    }
+//    func testRemoveInterquartileBounds() {
+//        let zero = [0.0, 0, 0]
+//
+//        XCTAssertEqual(zero.medianQuartileValues(), [0, 0, 0], "Failed to remove interquartile bounds of empty array")
+//
+//        let sorted = [1.0, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//
+//        XCTAssertEqual(sorted.medianQuartileValues(), [3, 4, 5, 6, 7, 8], "Failed to remove interquartile bounds of sorted array")
+//
+//
+//        let simple = [10.0,  8,  2,  3,  1]
+//
+//        XCTAssertEqual(simple.medianQuartileValues(), [2, 3, 8], "Failed to remove interquartile bounds of simple array")
+//
+//
+//        let complex = [93.0,  291,  205,  572,  175,  484,  456,  569,  33,  192,  245,  331,  126,  340,  424,  238,  156,  564,  516,  345,  195,  422,  10,  433,  112]
+//
+//        XCTAssertEqual(complex.medianQuartileValues(), [175, 192, 195, 205, 238, 245, 291, 331, 340, 345, 422, 424, 433], "Failed to remove interquartile bounds of complex array")
+//    }
     
     func testInterquartileMean() {
         let sorted = [1.0, 2, 3, 4, 5, 6, 7, 8, 9, 10]
