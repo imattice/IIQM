00e6e085f740697ba8f377bb5ceccea76f35ca37
Ike Mattice <ikemattice@icloud.com>
2021-02-22 07:32:55 -0800

    add a problem and solution statement to the original IIQM class; removed the definitions I added

--- IIQM/src/Deprecated/iiqm.swift
+++ IIQM/src/Deprecated/iiqm.swift
@@ -7,10 +7,19 @@
 //
 
 import Foundation
+//PROBLEM: IIQM lacks readability and clairity.
+//1) The name IIQM does not convey what the class is used for.  We can either get rid of it, or rename it.
+//2) The two functions contained are a bit unrelated and don't necessarily need to be grouped together in the same class.
+//3) The calculate:path method is very difficult to understand.  Variables are excessive and poorly named, there are no comments or descriptions, and it relies on the readFile:path method, which I don't think is necessary.
+
+//SOLUTIONS:
+//1) Move the calculate:path method to an Array<Int> extension.  This will allow us to act on the data directly, which will improve readability as well as give this functionality to any Array<Int> in the app without having to call a custom class.
+//2) Create a class that is designated for reading the text file.  That is a unique behavior that should be in its own class.  This also leaves open the potential to expand it if in the future we need to read other files.
+//3) Both the Array<Int> extension and the TextReader class can be easily tested.  We can develop these using TDD.
+//4) Deprecate the old IIQM class so that future developers do not use it. This also sets it up to be permanently removed in the future
 
 @available(*, deprecated, message: "Use IIQMCalculator instead")
 class IIQM {
-    ///Calculates the sum of all values in the data.txt file
     @available(*, deprecated, message: "Use Array<Double>.interquartileMean() instead")
     func calculate(path: String) {
         let lines = readFile(path: path)
